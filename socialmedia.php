d3ba7fdbdd85cacc9a9247f8bce12bc6b9afaaac
Venom <venom@venomslair.com>
2016-01-11 21:37:11 -0600

    Added Title, and removed my from the titles of the links to social media

--- socialmedia.php
+++ socialmedia.php
@@ -7,11 +7,15 @@
         <div style="display: none;"><a href="http://venomslair.com/crook.php">depth</a></div>
         <div id="menu"><?php include('menu.php'); ?></div>
         <div id="content">
+            <span class="titles">My Social Media Links</span><br />
+            <br />
             <a href="http://www.facebook.com/groups/venomslair">Facebook Group</a><br />
-            <a href="http://www.flickr.com/people/venom49637">My Photos</a><br />
-            <a href="http://www.youtube.com/venom49637">My YouTube</a><br />
-            <a href="http://www.twitch.tv/venom49637">My Twitch</a><br />
-            <a href="http://twitter.com/venom49637">My Twitter</a><br />
+            <a href="http://www.flickr.com/people/venom49637">Photos</a><br />
+            <a href="http://www.youtube.com/venom49637">YouTube</a><br />
+            <a href="http://www.twitch.tv/venom49637">Twitch</a><br />
+            <a href="http://twitter.com/venom49637">Twitter</a><br />
+            <br />
+            <br />
         <?php include('footer.php'); ?>
         </div>
     </body>
