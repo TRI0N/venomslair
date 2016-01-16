84307c88d9c77db65c510c8510b011255262d7fb
Venom <venom@venomslair.com>
2016-01-16 01:24:52 -0600

    forgot the <ul> tags

--- announcements.php
+++ announcements.php
@@ -9,8 +9,10 @@
 <span class="titles">Announcements</span><br />
 <br />
 <span class="boldundersm">01/15/2016</span><br />
+<ul>
 <li>I have made a dedicated weather page for all things weather</li>
 <li>I made a social media page to house all my social media contacts</li>
+</ul>
 <span class="boldundersm">11/6/2015</span><br />
 <ul>
 <li>I have decided that I am going to go back to this since its easier to update and makes sure that everyone see it.</li>
