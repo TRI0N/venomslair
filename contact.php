24f9add7afdff12d0b342bc0fe3d48b60be65672
Venom <venom@venomslair.com>
2016-01-27 10:00:09 -0800

    Fixed grammer and made it easier to read the code

--- contact.php
+++ contact.php
@@ -10,7 +10,9 @@
             <span class="titles">How to contact me</span>
             <br />
             <br />
-            If you want to make comments about my site, have any questions or need assistance, please click on the Helpdesk link on the menu on the left and submit your request there.  You can also come to our IRC server and see if I am available there.  Click on the IRC Information link to the left for information on how to connect.<br />
+            If you want to make comments about my site, have any questions or need assistance, please click on the Helpdesk link on
+            the menu on the left and submit your request there.  You can also come to our IRC server and see if I am available there,
+            for information on how to connect click on the IRC Information link to the left.<br />
             <br />
             <br />
             <br />
