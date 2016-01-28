8660478d5bf5f5a27862cb1072015a25ca588810
Venom <venom@venomslair.com>
2016-01-28 10:12:52 -0800

    Added IRC Logs for 1/27 for both channels

--- irclogs/wwiv/index.php
+++ irclogs/wwiv/index.php
@@ -16,9 +16,10 @@
 <br />
 <span class="titles">2016</span>
 <ul>
-    <li>01-27 (Wednesday) - Available Soon</li>
+    <li>01-28 (Thursday) - Available Soon</li>
     <!--Putting Link for logs when they go live
-    <li><a href="wwiv.log.20160127.html">01-27 (Wednesday)</a></li>-->
+    <li><a href="wwiv.log.20160128.html">01-28 (Thursday)</a></li>-->
+    <li><a href="wwiv.log.20160127.html">01-27 (Wednesday)</a></li>
     <li><a href="wwiv.log.20160126.html">01-26 (Tuesday)</a></li>
     <li><a href="wwiv.log.20160125.html">01-25 (Monday)</a></li>
     <li><a href="wwiv.log.20160124.html">01-24 (Sunday)</a></li>
