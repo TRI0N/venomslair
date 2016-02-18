2b2e142d3484e84e63eb86f7cc599b510b6ae24b
Venom <venom@venomslair.com>
2016-02-17 21:09:31 -0600

    Made some other changes for HTML5

--- header.php
+++ header.php
@@ -1,18 +1,10 @@
 <!doctype html>
-<html>
+<html lang="en">
     <head>
+        <meta charset="utf-8">
         <title>My Homepage</title>   
-        <link rel="stylesheet" href="../css/mySTYLE.css" type="text/css" media="screen" />
-        <!--<style type="text/css" media="print">@import url(../css/myPRINTSTYLE.css);</style>-->
-        <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-        <meta http-equiv="expires" content="-1" />
-        <meta http-equiv= "pragma" content="no-cache" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="author" content="Venom"/>
-        <meta name="robots" content="all" />
-        <meta name="MSSmartTagsPreventParsing" content="true" />
-        <meta name="description" content="This is My Home Page" />
-        <meta name="keywords" content="Home Pages" />
+        <link rel="stylesheet" href="../css/mySTYLE.css" type="text/css">
+        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![end if]-->
         <div id="mast">
         <img src="../navimages/mast.jpg" width="700" height="100" border="0" alt="mast.jpg" />
         </div>
