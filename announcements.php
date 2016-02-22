d88cc31cd6ac359029315edfc6cf3d8557eeb6fa
Venom <venom@venomslair.com>
2016-02-21 18:01:11 -0600

    Added announcements for 2/21

--- announcements.php
+++ announcements.php
@@ -1,6 +1,11 @@
 <?php include('header.php'); ?>
 <span class="titles">Announcements</span><br />
 <br />
+<span class="boldundersm">02/21/2016</span><br />
+<ul>
+<li>I have made some improvements to the load time of the page</li>
+<li>I have also changed the way you can read the IRC Logs and they are much easier to maintain</li>
+</ul>
 <span class="boldundersm">01/15/2016</span><br />
 <ul>
 <li>I have made a dedicated weather page for all things weather</li>
